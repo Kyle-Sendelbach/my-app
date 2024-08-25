959e2a8d235c3564008fb6c560c326d3a14a9633
Kyle-Sendelbach <sendelbach.18@osu.edu>
2024-08-25 17:23:27 -0400

    Delete MyConsoleApp directory

--- MyConsoleApp/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
